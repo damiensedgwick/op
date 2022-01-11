7ac57a75b41c1523a1c7ed53e045d50bcb968651
Damien Sedgwick <damiensedgiwck@icloud.com>
2022-01-11 17:04:20 +0000

    Basic commands working

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/cli/browser"
 	"github.com/damiensedgwick/op/utils"
+	"log"
+	"os"
 )
 
 func main() {
+	arg := os.Args[1]
+	//flags := os.Args[2:]
+
+	fmt.Println("Searching for " + arg + " documents...")
+	fmt.Println(arg + " documents found, opening browser...")
+
+	err := browser.OpenURL(utils.ReturnCorrectLink(arg))
+	if err != nil {
+		log.Fatal("Could not open documents")
+	}
+
 	// when completed, the goal is to open the link in the browser and print it for reference.
-	fmt.Println(utils.ReturnCorrectLink("react"))
+	//fmt.Println(utils.ReturnCorrectLink(arg))
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/damiensedgwick/op
 go 1.17
 
 require (
+	github.com/cli/browser v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.1.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20210319071255-635bc2c9138d // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
