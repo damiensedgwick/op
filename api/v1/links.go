6242345d54ad1025cc5e09d3c7428acc3a480f87
Damien Sedgwick <damiensedgiwck@icloud.com>
2022-01-11 21:12:00 +0000

    added a few links

---
+++ api/v1/links.go
@@ -0,0 +1,82 @@
+package v1
+
+var links = map[string]string{
+	// a
+	"angular": "https://angular.io/docs",
+	// b
+	"bash":      "https://devdocs.io/bash/",
+	"bootstrap": "https://getbootstrap.com/docs/4.1/getting-started/introduction/",
+	// c
+	"c":            "https://docs.microsoft.com/en-us/cpp/?view=msvc-170",
+	"c++":          "https://docs.microsoft.com/en-us/cpp/?view=msvc-170",
+	"cpp":          "https://docs.microsoft.com/en-us/cpp/?view=msvc-170",
+	"c#":           "https://docs.microsoft.com/en-us/dotnet/csharp/",
+	"csharp":       "https://docs.microsoft.com/en-us/dotnet/csharp/",
+	"clojure":      "https://clojure.org/guides/getting_started",
+	"coffeescript": "https://coffeescript.org/",
+	// d
+	"docker": "https://docs.docker.com/",
+	"dotnet": "https://docs.microsoft.com/en-us/dotnet/",
+	"drupal": "https://www.drupal.org/documentation",
+	// e
+	// f
+	"f#":     "https://docs.microsoft.com/en-us/dotnet/fsharp/",
+	"fsharp": "https://docs.microsoft.com/en-us/dotnet/fsharp/",
+	// g
+	"gatsby":   "https://www.gatsbyjs.com/docs/",
+	"ghost":    "https://ghostcms.net/",
+	"ghostcms": "https://ghostcms.net/",
+	"go":       "https://go.dev/doc/",
+	"golang":   "https://go.dev/doc/",
+	"graphql":  "https://graphql.org/learn/",
+	"gql":      "https://graphql.org/learn/",
+	// h
+	"haskel": "https://www.haskell.org/documentation/",
+	"hugo":   "https://gohugo.io/documentation/",
+	// i
+	// j
+	"javascript": "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+	"jquery":     "https://api.jquery.com/",
+	// k
+	// l
+	"laravel": "https://laravel.com/docs/8.x/installation",
+	// m
+	// n
+	"next":   "https://nextjs.org/docs/getting-started",
+	"nextjs": "https://nextjs.org/docs/getting-started",
+	// o
+	"objc":  "https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/ProgrammingWithObjectiveC/Introduction/Introduction.html",
+	"obj-c": "https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/ProgrammingWithObjectiveC/Introduction/Introduction.html",
+	// p
+	"purescript": "https://book.purescript.org/",
+	"php":        "https://www.php.net/docs.php",
+	"python":     "https://docs.python.org/3/",
+	// q
+	// r
+	"react":    "https://reactjs.org/docs/getting-started.html",
+	"rescript": "https://rescript-lang.org/",
+	"redux":    "https://redux.js.org/",
+	"relay":    "https://relay.dev/docs/",
+	// s
+	"sanity": "https://www.sanity.io/docs/getting-started",
+	"strapi": "https://strapi.io/",
+	// t
+	"tailwind":    "https://tailwindcss.com/docs/installation",
+	"tailwindcss": "https://tailwindcss.com/docs/installation",
+	"typescript":  "https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html",
+	// u
+	// v
+	// w
+	"wp":        "https://codex.wordpress.org/Main_Page",
+	"wordpress": "https://codex.wordpress.org/Main_Page",
+	// x
+	"xstate": "https://xstate.js.org/docs/",
+	// y
+	// z
+	// #
+	".net": "https://docs.microsoft.com/en-us/dotnet/",
+}
+
+func Links() map[string]string {
+	return links
+}
--- utils/correct_link.go
+++ utils/correct_link.go
@@ -1,12 +1,10 @@
 package utils
 
-var links = map[string]string{
-	"javascript": "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-	"typescript": "https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html",
-	"react":      "https://reactjs.org/docs/getting-started.html",
-}
+import v1 "github.com/damiensedgwick/op/api/v1"
 
 func ReturnCorrectLink(q string) string {
+	links := v1.Links()
+
 	res := ""
 
 	for k, v := range links {
